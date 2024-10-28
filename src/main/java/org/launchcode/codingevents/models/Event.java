6a9fdb494673bdfc65e370c7d048d556426692f3
Carddera1842 <carddera1842@gmail.com>
2024-10-28 15:40:27 -0500

    "enum"

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,12 +3,12 @@ package org.launchcode.codingevents.models;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
 import java.util.Objects;
 
 /**
  * Created by Chris Bay
  */
+
 public class Event {
 
     private int id;
